4a9c638aca43e2e842d43527c1a14fd48a943281
rpidanny <abhishekmaharjan1993@gmail.com>
2019-02-10 09:09:18 +0100

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const { mineQuotes } = require('./miner')
-const { tweet } = require('./twitter')
 const { success, failure } = require('./utils/responses')
 
 module.exports.test = (event, context, callback) => {
   return callback(null, success({ data: 'OK' }))
 }
 
-module.exports.mine = async (event, context, callback) => {
+module.exports.mine = (event, context, callback) => {
+  const { mineQuotes } = require('./miner')
   mineQuotes()
     .then(() => callback(null, success({ data: 'OK' })))
     .catch(err => callback(null, failure({ err })))
 }
 
 module.exports.tweet = (event, context, callback) => {
+  const { tweet } = require('./twitter')
   tweet()
     .then(data => callback(null, success({ data })))
     .catch(err => callback(null, failure({ err })))
