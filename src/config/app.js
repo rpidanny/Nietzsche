1218183293e25fe7c9ae307305446d12a110c54a
rpidanny <abhishekmaharjan1993@gmail.com>
2019-02-21 14:54:18 +0100

    Fix lambda readonly fs

--- src/config/app.js
+++ src/config/app.js
@@ -2,6 +2,9 @@ const path = require('path')
 
 const rootPath = path.normalize(`${__dirname}/../..`)
 
+const tmpPath = '/tmp'
+
 module.exports = {
-  rootPath
+  rootPath,
+  tmpPath
 }
--- src/utils/image.js
+++ src/utils/image.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const { promisify } = require('util')
 
 const { execCmd } = require('./')
-const { rootPath } = require('../config/app')
+const { rootPath, tmpPath } = require('../config/app')
 
 const readFile = promisify(fs.readFile)
 
@@ -15,6 +15,7 @@ const imageToB64 = image => new Promise((resolve, reject) => {
 const generateImage = (quote, background) => new Promise((resolve, reject) => {
   const { text, author } = quote
   const string = `"\u201C${text}\u201D\n\n-${author}"`
+  const outputImage = `${tmpPath}/${new Date().getTime()}.png`
   const cmd = `printf ${string} | \
   convert \
   -size 399x399 \
@@ -26,10 +27,10 @@ const generateImage = (quote, background) => new Promise((resolve, reject) => {
   -compose over \
   -composite ${rootPath}/src/assets/images/${background}  \
   caption:@- \
-  quote.png`
+  ${outputImage}`
   execCmd(cmd)
     .then(() => resolve({
-      path: `${rootPath}/quote.png`
+      path: outputImage
     }))
     .catch(err => reject(err))
 })
