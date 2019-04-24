dbb7f72851b6052051fbb075f306d94a5310793e
rpidanny <abhishekmaharjan1993@gmail.com>
2019-04-24 20:01:55 +0200

    Add twitter files

---
+++ src/twitter.js
@@ -0,0 +1,63 @@
+const dynamoDb = require('./utils/dynamodb')
+const { success, failure } = require('./utils/responses')
+
+const getRandomQuote = () => new Promise((resolve, reject) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    FilterExpression: '#notified = :flag AND #likes > :likes',
+    ExpressionAttributeNames: {
+      '#notified': 'notified',
+      '#likes': 'likes'
+    },
+    ExpressionAttributeValues: {
+      ':flag': false,
+      ':likes': 5000
+    },
+    Limit: 10000
+  }
+  dynamoDb.scan(params, (err, result) => {
+    if (err) {
+      reject(err)
+    } else {
+      const { Items, Count } = result
+      const index = Math.floor(Math.random() * Count)
+      const quote = Items[index]
+      console.log(result)
+      resolve(quote)
+    }
+  })
+})
+
+const getRandomQuoteQuery = () => new Promise((resolve, reject) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    IndexName: 'likes-index',
+    KeyConditionExpression: '#likes > :likes',
+    ExpressionAttributeNames: {
+      // '#notified': 'notified',
+      '#likes': 'likes'
+    },
+    ExpressionAttributeValues: {
+      // ':flag': false,
+      ':likes': 5000
+    },
+    Limit: 100
+  }
+  dynamoDb.query(params, (err, result) => {
+    if (err) {
+      reject(err)
+    } else {
+      const { Items, Count } = result
+      const index = Math.floor(Math.random() * Count)
+      const quote = Items[index]
+      console.log(result)
+      resolve(quote)
+    }
+  })
+})
+
+module.exports.tweetQuote = (event, context, callback) => {
+  getRandomQuoteQuery()
+    .then(quote => callback(null, success(JSON.stringify(quote, null, 2))))
+    .catch(err => callback(err))
+}
