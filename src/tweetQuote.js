d949f28457789bf579c1e6c7556cd474c5e431ff
rpidanny <abhishekmaharjan1993@gmail.com>
2019-04-26 14:32:18 +0200

    Add functions to quote tweet state machine

--- src/tweetQuote.js
+++ src/tweetQuote.js
@@ -4,33 +4,26 @@ const { formatTweet, postTweet, postImageTweet } = require('./utils/twitter')
 const { generateImage, imageToB64 } = require('./utils/image')
 
 module.exports.handler = (event, context, callback) => {
-  // const quote = event.quote
-  const quote = {
-    likes: 2417,
-    updatedAt: '1556205736471',
-    createdAt: '1556205736471',
-    text:
-      'When the snows fall and the white winds blow, the lone wolf dies but the pack survives.',
-    used: 0,
-    quoteId: '214e857d1df74a32b7141f532514d16d',
-    tags: ['friendship', 'loneliness'],
-    author: 'George R.R. Martin'
-  }
+  const quote = event
 
-  if (quote.text.length + quote.author.length <= maxTweetLength + 3) {
-    postTweet({
-      status: formatTweet(quote)
-    })
-      .then(() => callback(null, quote))
-      .catch(callback)
+  if (quote) {
+    if (quote.text.length + quote.author.length <= maxTweetLength + 3) {
+      postTweet({
+        status: formatTweet(quote)
+      })
+        .then(() => callback(null, quote))
+        .catch(callback)
+    } else {
+      generateImage({
+        text: quote.text,
+        author: quote.author.replace(/,/g, '')
+      }, 'bg2.jpg')
+        .then(imageToB64)
+        .then(postImageTweet)
+        .then(() => callback(null, quote))
+        .catch(callback)
+    }
   } else {
-    generateImage({
-      text: quote.text,
-      author: quote.author.replace(/,/g, '')
-    }, 'bg2.jpg')
-      .then(imageToB64)
-      .then(postImageTweet)
-      .then(() => callback(null, quote))
-      .catch(callback)
+    callback(new Error('Invalid Data'))
   }
 }
