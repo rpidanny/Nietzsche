8283b35eb14937f27e762ea097d2f7b062f3cdf9
rpidanny <abhishekmaharjan1993@gmail.com>
2019-04-26 12:20:15 +0200

    Add getRandomQuote function

---
+++ src/getRandomQuote.js
@@ -0,0 +1,41 @@
+const dynamoDb = require('./utils/dynamodb')
+
+const { likesThreshold, preferredAuthors } = require('./config/quotes')
+
+const index = Math.floor(Math.random() * preferredAuthors.length)
+
+module.exports.handler = (event, context, callback) => {
+  const author = preferredAuthors[index]
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    KeyConditionExpression: '#author = :author',
+    FilterExpression: '#likes >= :likes AND #used <= :used',
+    ExpressionAttributeNames: {
+      '#author': 'author',
+      '#likes': 'likes',
+      '#used': 'used'
+    },
+    ExpressionAttributeValues: {
+      ':author': author,
+      ':likes': likesThreshold,
+      ':used': process.env.QUOTE_USED_THRESHOLD || 0
+    },
+    Limit: 20
+  }
+  dynamoDb.query(params, (err, result) => {
+    const { Items, Count } = result
+    if (err) {
+      callback(err)
+    } else {
+      if (Count > 0) {
+        const index = Math.floor(Math.random() * Count)
+        const quote = Items[index]
+        console.log(`Found ${result.Count} quotes from ${author}`)
+        callback(null, quote)
+      } else {
+        callback(new Error('No quote from author'))
+      }
+    }
+  })
+}
