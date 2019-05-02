06a1f162c0081f50e2d2a42b93b41e15cfd9b067
rpidanny <abhishekmaharjan1993@gmail.com>
2019-05-02 14:03:46 +0200

    Add image quote tweet when long text

--- src/tweetQuote.js
+++ src/tweetQuote.js
@@ -1,8 +1,8 @@
 const { maxTweetLength } = require('./config/twitter')
 
-const { LongQuoteError } = require('./utils/customErrors')
-const { formatTweet, postTweet } = require('./utils/twitter')
-// const { generateImage, imageToB64 } = require('./utils/image')
+// const { LongQuoteError } = require('./utils/customErrors')
+const { formatTweet, postTweet, postImageTweet } = require('./utils/twitter')
+const { getRandomImage, addQuoteToImage, imageToB64 } = require('./utils/image')
 
 module.exports.handler = (event, context, callback) => {
   const quote = event
@@ -15,15 +15,16 @@ module.exports.handler = (event, context, callback) => {
         .then(() => callback(null, quote))
         .catch(callback)
     } else {
-      callback(new LongQuoteError('Quote too long to tweet.'))
-      // generateImage({
-      //   text: quote.text,
-      //   author: quote.author.replace(/,/g, '')
-      // }, 'bg2.jpg')
-      //   .then(imageToB64)
-      //   .then(postImageTweet)
-      //   .then(() => callback(null, quote))
-      //   .catch(callback)
+      // callback(new LongQuoteError('Quote too long to tweet.'))
+      getRandomImage()
+        .then(img => addQuoteToImage({
+          text: quote.text,
+          author: quote.author.replace(/,/g, '')
+        }, img.path))
+        .then(imageToB64)
+        .then(postImageTweet)
+        .then(() => callback(null, quote))
+        .catch(callback)
     }
   } else {
     callback(new Error('Invalid Data'))
--- src/utils/image.js
+++ src/utils/image.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const request = require('request-promise')
+
 const { promisify } = require('util')
 
 const { execCmd } = require('./')
@@ -6,26 +8,29 @@ const { rootPath, tmpPath } = require('../config/app')
 
 const readFile = promisify(fs.readFile)
 
+const download = (uri, filename, callback) => {
+  request.head(uri, (err, res, body) => {
+    if (err) {
+      callback(err)
+    } else {
+      console.log('content-type:', res.headers['content-type'])
+      console.log('content-length:', res.headers['content-length'])
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback)
+    }
+  })
+}
+
 const imageToB64 = image => new Promise((resolve, reject) => {
   readFile(image.path)
     .then(file => resolve(file.toString('base64')))
     .catch(err => reject(err))
 })
 
-const generateImage = (quote, background) => new Promise((resolve, reject) => {
-  const { text, author } = quote
-  const string = `"\u201C${text}\u201D\n\n-${author}"`
-  const outputImage = `${tmpPath}/${new Date().getTime()}.png`
+const blurBackground = (background, outputImage) => new Promise((resolve, reject) => {
   const cmd = `convert \
-  -size 399x399 \
-  -fill "#F5E5FC" \
-  -font Times-New-Roman \
-  -pointsize 18 \
-  -background transparent \
-  -gravity center \
-  -compose over \
-  -composite ${rootPath}/assets/backgrounds/${background}  \
-  caption:${string} \
+  ${background} \
+  -filter Gaussian \
+  -blur 0x8 \
   ${outputImage}`
   execCmd(cmd)
     .then(() => resolve({
@@ -34,7 +39,48 @@ const generateImage = (quote, background) => new Promise((resolve, reject) => {
     .catch(err => reject(err))
 })
 
+const addQuoteToImage = (quote, background) => new Promise((resolve, reject) => {
+  const { text, author } = quote
+  const string = `"\u201C${text}\u201D\n\n-${author}"`
+  const outputImage = `${tmpPath}/${new Date().getTime()}.png`
+  const cmd = `convert \
+  -size 1800x960 \
+  -fill "#F5E5FC" \
+  -font "DejaVu-Sans-Oblique" \
+  -pointsize 36 \
+  -background transparent \
+  -undercolor black \
+  -gravity center \
+  -compose over \
+  -composite ${outputImage}  \
+  caption:${string} \
+  ${outputImage}`
+
+  blurBackground(background, outputImage)
+    .then(() => execCmd(cmd))
+    .then(() => resolve({
+      path: outputImage
+    }))
+    .catch(err => reject(err))
+})
+
+const getRandomImage = new Promise((resolve, reject) => {
+  const path = `${tmpPath}/${new Date().getTime()}_neitzsche_random.jpg`
+  download(
+    'https://unsplash.it/1920/1080/?random',
+    path,
+    err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ path })
+      }
+    })
+})
+
 module.exports = {
-  generateImage,
+  blurBackground,
+  addQuoteToImage,
+  getRandomImage,
   imageToB64
 }
