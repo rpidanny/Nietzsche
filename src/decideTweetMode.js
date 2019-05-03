713e38f3c2a0e6c99c84515dd7a820ad3e450212
rpidanny <abhishekmaharjan1993@gmail.com>
2019-05-03 09:32:12 +0200

    Add probability tweet mode decision function

---
+++ src/decideTweetMode.js
@@ -0,0 +1,27 @@
+const { maxTweetLength } = require('./config/twitter')
+const { NoQuoteError } = require('./utils/customErrors')
+
+/**
+ * Random variable to decide either to tweet text or image
+ * Probability of text = 3/4
+ * Probability of image = 1/4
+ * */
+const getTweetChoice = () => {
+  const randomNumber = Math.floor(Math.random() * Math.floor(4))
+  return randomNumber < 3
+}
+
+module.exports.handler = (event, context, callback) => {
+  const { text, author } = event
+  console.log(event)
+  if (text && author) {
+    const tweetImageFlag = getTweetChoice()
+    const tweetLength = event.text.length + event.author.length + 5
+    callback(null, {
+      ...event,
+      mode: tweetLength <= maxTweetLength && tweetImageFlag ? 'text' : 'image'
+    })
+  } else {
+    callback(new NoQuoteError('No Quote in input to function.'))
+  }
+}
