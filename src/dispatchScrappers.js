291211b83d8ec1f048e578c726e5c89bd246d9a2
rpidanny <abhishekmaharjan1993@gmail.com>
2020-11-07 19:35:06 +0100

    refactor: use nietzsche.js to fetch quotes from goodreads

--- src/dispatchScrappers.js
+++ src/dispatchScrappers.js
@@ -1,25 +1,16 @@
 const AWS = require('aws-sdk')
-const { baseUrl, startPage, maxPage, routes } = require('./config/goodreads')
+const { routes } = require('./config/goodreads')
 
 const sns = new AWS.SNS()
 
-const getPageUrl = (route, page) => `${baseUrl}${route}?format=json&page=${page}`
-
 module.exports.handler = (event, context, callback) => {
   const { SNS_ARN } = process.env
-  const pages = []
-  // const maxPage = 1
-  routes.forEach((route, idx) => {
-    for (let i = startPage; i <= maxPage; i++) {
-      pages.push(getPageUrl(route, i))
-    }
-  })
 
   Promise
-    .all(pages.map(pageUrl => new Promise((resolve, reject) => {
+    .all(routes.map(route => new Promise((resolve, reject) => {
       const params = {
         Message: 'Scrap',
-        Subject: pageUrl,
+        Subject: route,
         TopicArn: SNS_ARN
       }
       sns.publish(params, (err, data) => {
